4bd9b7fa4a1a6cf9917840275982bacb7213743b
RonaldKwan <ronaldoskates@gmail.com>
2022-05-04 22:32:08 -0400

    commit tree d4b9dfc68eb75682265c455f7d6795c5b92245f9

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+if(someNumber <= 400) {
+  return 'This one is on me!'
+}
+else if(someNumber <= 2000){
+  return 'That will be twenty bucks.'
+}
+else if(someNumber <= 2500){
+  return 'I will gladly take your thirty bucks.'
+}
+else {
+  return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+// if(someCity == 'NYC') {
+//   return 'Ok, sounds good.'}
+// else if(someCity != 'NYC') {
+//   return 'No go.'}  
+return someCity == 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(generous){
   // Write your code here!
-}
+  const generousness = "generosity";
+
+  let isGenerous, isnotasGenerous, anythingElse;
+
+  switch (generousness) {
+    case '':
+      isGenerous = generous
+      break;
+    default:
+      console.log("Thank you so much.");
+      break;
+  }
+
+
+
+  }
